441fe320650d02c9545a681b221134868c3752a4
Desiree <dcox@zendesk.com>
2016-05-05 15:49:29 -0700

    Add features association to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
+  has_many :features
   has_many :metrics, through: :features
 
   validates_with EmailValidator
