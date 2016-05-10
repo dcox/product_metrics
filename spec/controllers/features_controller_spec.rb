e4a5373743368224fa18ae3433f1cf1b50f87667
Desiree <dcox@zendesk.com>
2016-05-10 13:24:19 -0700

    Add Devise test helpers.

--- spec/controllers/features_controller_spec.rb
+++ spec/controllers/features_controller_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
-include Devise::TestHelpers
 
 RSpec.describe FeaturesController, type: :controller do
   describe "GET new" do
     it "renders the new template" do
+      sign_in FactoryGirl.create(:user)
       get :new
       expect(response).to render_template("new")
     end
   end
-
 end
